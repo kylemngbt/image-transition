d90d02b378f197835a83f1562cd34b16228f3a4a
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-18 19:13:20 +0800

    build: create a reference

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 const imgBox = document.querySelector(".img-box");
 const imgWrap = document.querySelector(".img-wrap");
+const originalImg = document.querySelector("#original-img");
 
 let leftSpace = imgBox.offsetLeft;
 
