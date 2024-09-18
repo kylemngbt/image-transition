081f62678edaf5471face7150fea487d5fd5aa57
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-18 19:16:07 +0800

    build: fix the original width to the imgbox width

--- javascript/script.js
+++ javascript/script.js
@@ -2,5 +2,7 @@ const imgBox = document.querySelector(".img-box");
 const imgWrap = document.querySelector(".img-wrap");
 const originalImg = document.querySelector("#original-img");
 
+originalImg.style.width = imgBox.offsetWidth + "px";
+
 let leftSpace = imgBox.offsetLeft;
 
